f49ebd527ab23bd365b22f97a236b91bdc1571a5
Clement Cornu <clement.cornu@cpe.fr>
2021-05-02 12:10:29 +0200

    Fin classe échiquier, manque tests

--- src/model/Echiquier.java
+++ src/model/Echiquier.java
@@ -1,8 +1,12 @@
 package model;
-import java.model.Jeu;
+import model.Jeu;
+import model.PieceIHM;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Echiquier extends java.lang.Object implements BoardGames {
-	// Noms ï¿½ vï¿½rifier
+	// Noms a verifier
 	private Jeu jeuNoir;
 	private Jeu jeuBlanc;
 	//Pas besoin de getter et setter car on a switchJoueur : on sait quel est le jeu courant et le jeu non courant
@@ -16,9 +20,8 @@ public class Echiquier extends java.lang.Object implements BoardGames {
 	}
 	
 	@Override
-	public Couleur getColorCurrentPlayer() {
-		// TODO 
-		return null;
+	public Couleur getColorCurrentPlayer() { 
+		return this.joueurCourant;
 	}
 	
 	@Override
@@ -28,16 +31,27 @@ public class Echiquier extends java.lang.Object implements BoardGames {
 	
 	@Override
 	public Couleur getPieceColor(int x, int y) {
-		// TODO 
-		return null;
+		return this.jeuBlanc.getPieceColor(x,y);
+	}
+	
+	public List<PieceIHM> getPiecesIHM(){
+		//TODO faire valider le fonctionnement
+		List<PieceIHM> PieceIHM = new ArrayList<PieceIHM>(); //TODO vérifier si on a besoin d'une arrayList ou si un autre type nous conviendrait
+		Jeu Jeu = this.jeuBlanc;
+		for (int i=0;i<Jeu.getPieceIHM().size();i++) {
+			PieceIHM.add(Jeu.getPieceIHM().get(i)); //On récupère le i-eme élément
+		}
+		Jeu Jeu = this.jeuNoir;
+		for (int i=0;i<Jeu.getPieceIHM().size();i++) {
+			PieceIHM.add(Jeu.getPieceIHM().get(i)); //On récupère le i-eme élément
+		}
+		//TODO vérifier comment donner des accès en écriture
+		return PieceIHM;
 	}
 	
 	private void setMessage(String newMessage) {
 		message = newMessage;
 	}
-	
-	
-	
 	
 	@Override
 	public boolean isEnd() {
@@ -57,7 +71,7 @@ public class Echiquier extends java.lang.Object implements BoardGames {
 		//il n'existe pas de piece du jeu courant aux coordonnees initiales
 		// TODO 
 		// vérifier si condition sur jeu courant
-		if (!(Jeu.isPieceHere(xInit,yInit) && Jeu.getPieceColor() != this.joueurCourant)) {
+		if (!(Jeu.isPieceHere(xInit,yInit) && this.getPieceColor(xInit,yInit) != this.joueurCourant)) {
 			ret = false;
 		}
 		// les coordonnees finales ne sont pas valides ou egales aux initiales
@@ -71,7 +85,7 @@ public class Echiquier extends java.lang.Object implements BoardGames {
 		}
 		//il existe une piece intermediaire sur la trajectoire (sauf cavalier)
 		//TODO trouver la bonne fonction
-		else if (Jeu.move()) {
+		else if (Jeu.intermediatePiece()) {
 			if 	(Jeu.getPieceType(xInit,yInit).equals("Cavalier")) {
 				ret = true;
 			}
@@ -81,23 +95,23 @@ public class Echiquier extends java.lang.Object implements BoardGames {
 		else if (Jeu.isPieceHere(xFinal,yFinal)) {
 			//si elle est de la meme couleur
 			if (Jeu.getPieceColor(xInit,yInit) == (Jeu.getPieceColor(xFinal,yFinal))){
-				if (Jeu.setCastling()) { s
+				if (Jeu.setCastling()) {
 				ret = true;
 				}
 				ret = false;
 			}
 			//sinon prendre la piece intermediaire (vigilance pour le cas du pion) et deplacer la piece
-			else if (Jeu.setPossibleCapture()) {
+			else {
 				if (Jeu.isPawnPromotion(xFinal,yFinal)) {
 					//TODO vérifier comment mettre en place le type
 					Jeu.pawnPromotion(xFinal,yFinal,type);
 				}
-				ret = Jeu.capture(xFinal,yFinal);
+				ret = Jeu.setPossibleCapture();//ret = true si capture possible
 			}
 		}
 		//sinon deplacer la piece
 		else {
-			ret = this.move(xInit,yInit,xFinal,yFinal);//ret = true si déplacement effectué	
+			ret = true;
 		}
 
 		return ret;
@@ -105,8 +119,35 @@ public class Echiquier extends java.lang.Object implements BoardGames {
 
 	@Override
 	public boolean move(int xInit, int yInit, int xFinal, int yFinal) {
-		// TODO
-		return false;
+		//TODO implémentation de checkmatePossibility ?
+		boolean ret;
+		Jeu Jeu;
+		if (this.getColorCurrentPlayer() == Couleur.BLANC) {
+			Jeu = this.jeuBlanc;
+		}
+		else {
+			Jeu = this.jeuNoir;
+		}
+		Pieces Piece = Jeu.findPiece(xInit,yInit); //Recup la pièce a la pos (xinit,yinit) afin de lui faire effectuer le déplacement
+		if (this.isMoveOk(xInit, yInit, xFinal, yFinal)) {
+			Piece.move(xFinal,yFinal);
+			if (Jeu.capturePossible) {
+				Jeu.capture(xFinal,yFinal);
+			}
+			ret = true;//Jusque la et si pas de possibilité de mise en échec, le déplacement est effectué
+			Coord KingCoord = Jeu.getKingCoord();
+			if (Jeu.checkmatePossibility()){ //Etude des possibilités de mise en échec
+				Jeu.undoMove();
+				if (Jeu.capturePossible) {
+					Jeu.undoCapture();
+				}
+				ret = false;
+			}
+		}
+		else {
+			ret = false;
+		}
+		return ret;
 	}
 	
 	public void switchJoueur() {
