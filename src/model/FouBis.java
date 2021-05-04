d74cd3186bdc0c38a9054ef07d16122a57e3a52b
jujudel04 <78476197+jujudel04@users.noreply.github.com>
2021-05-04 16:56:01 +0200

    Update FouBis.java

--- src/model/FouBis.java
+++ src/model/FouBis.java
@@ -1,22 +1,23 @@
 package model;
 
-public class FouBis extends AbstractPiece {
-	public FouBis(Couleur couleur, Coord coord) {
+public class Fou extends AbstractPiece {
+	public Fou(Couleur couleur, Coord coord) {
 		super(couleur, coord);
-
+		super.name = "fou";
 	}
 
 	@Override
 	public boolean isMoveOk(int xFinal, int yFinal) {
-		int sommeXY = super.getX() + super.getY();
-		int sommexy_final = xFinal + yFinal;
-		int sousXY = super.getX() - super.getY();
-		int sousxy_final = xFinal - yFinal;
-		if ((sommeXY == sommexy_final) || (sousXY == sousxy_final)) {
-			return true;
-		} else {
-			return false;
+		int sommeX = xFinal - super.getX();
+		int sommeY = yFinal - super.getY();
+		for (int i = 0; i < 8;) {
+			if (((sommeX == i) && (sommeY == i)) || ((sommeX == -i) && (sommeY == -i))
+					|| ((sommeX == i) && (sommeY == -i)) || ((sommeX == -i) && (sommeY == i))) {
+				return true;
+			}
+			i++;
 		}
-
+		return false;
 	}
 }
+
