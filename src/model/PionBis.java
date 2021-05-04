7950e645a627f89567903413410f086fda140e3c
jujudel04 <78476197+jujudel04@users.noreply.github.com>
2021-05-04 15:18:41 +0200

    Create PionBis.java

---
+++ src/model/PionBis.java
@@ -0,0 +1,54 @@
+package model;
+
+/**
+ * @author francoise.perrin Inspiration Jacques SARAYDARYAN, Adrien GUENARD *
+ * 
+ */
+
+public class Pion extends AbstractPiece {
+
+	public Pion(Couleur couleur_de_piece, Coord coord) {
+		super(couleur_de_piece, coord);
+		/*
+		 * Constructeur
+		 */
+	}
+
+	public boolean isMoveOk(int xFinal, int yFinal) {
+		int X = xFinal - this.getX();
+		int Y = xFinal - this.getY();
+		int mov = 0;
+		if (this.getCouleur() == Couleur.NOIR) {
+			if (this.getY() == 6) {
+				if (Y == -1 || Y == -2) {
+					mov = 1;
+				}
+			} else {
+				if (Y == -1) {
+					mov = 1;
+				}
+			}
+		}
+		if (this.getCouleur() == Couleur.BLANC) {
+			if (this.getY() == 1) {
+				if (Y == 1 || Y == 2) {
+					mov = 1;
+				}
+			} else {
+				if (Y == 1) {
+					mov = 1;
+				}
+			}
+		}
+		if (mov == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+/*
+ * returns true si déplacement légal en fonction des algo de déplacement
+ * spécifique de chaque pièce
+ */
