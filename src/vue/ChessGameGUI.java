5ee764881f71b7b80e821f4cd371b8e11cd70642
Clement Cornu <clement.cornu@cpe.fr>
2021-05-05 08:29:10 +0200

    Start partie 4

--- src/vue/ChessGameGUI.java
+++ src/vue/ChessGameGUI.java
@@ -1,4 +1,60 @@
 package vue;
 
-public class ChessGameGUI {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+public class ChessGameGUI extends JFrame implements MouseListener,MouseMotionListener,Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
