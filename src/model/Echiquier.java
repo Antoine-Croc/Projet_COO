ab1ba8fb2c3d46f5335216db5baf798564bb0a78
GUO-Yong <yongbinfrance@gmail.com>
2021-05-11 18:23:21 +0200

    update intermediare

--- src/model/Echiquier.java
+++ src/model/Echiquier.java
@@ -5,6 +5,8 @@ import model.PieceIHM;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Echiquier extends java.lang.Object implements BoardGames {
 	// Noms a verifier
